52df1dd31b997bbc6c5a8cd9aea09bfd15cabab8
Viach <vch.380@gmail.com>
2016-11-16 18:19:44 +0200

    quiz initial (created models. etc)

---
+++ quiz/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class CategoryQuestion(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+class Answer(models.Model):
+    name = models.CharField(max_length=200)
+    explanation = models.TextField()
+    code = models.TextField()
+    category = models.ForeignKey(CategoryQuestion)
+
+class Question(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    code = models.TextField()
+    category = models.ForeignKey(CategoryQuestion)
+    correct_answer = models.ForeignKey(Answer)
+
+class UsefulLinks(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    link = models.CharField(max_length=200)
