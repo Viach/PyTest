f63b44530291adbf9697afe687ce187644d6ceac
Viach <vch.380@gmail.com>
2016-11-21 18:08:14 +0200

    added class Quiz

--- quiz/models.py
+++ quiz/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+
 class CategoryQuestion(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 
 class Answer(models.Model):
     name = models.CharField(max_length=200)
@@ -10,12 +15,29 @@ class Answer(models.Model):
     code = models.TextField()
     category = models.ForeignKey(CategoryQuestion)
 
+    def __str__(self):
+        return self.name
+
+
 class Question(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     code = models.TextField()
     category = models.ForeignKey(CategoryQuestion)
     correct_answer = models.ForeignKey(Answer)
+    user_answer = None
+
+    def __str__(self):
+        return self.name
+
+
+class Quiz():
+    def __init__(self, n=2):
+        self.questions = Question.objects.all()[:n]
+
+    def __str__(self):
+        return 'Set of Questions'
+
 
 class UsefulLinks(models.Model):
     name = models.CharField(max_length=200)
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Quiz
 
 
 def index(request):
@@ -7,7 +8,8 @@ def index(request):
 
 
 def quiz(request):
-    context = {'var': 'quiz QUIZ', }
+    quiz = Quiz()
+    context = {'quiz': quiz , 'test': 'TEST'}
     return render(request, 'quiz/quiz.html', context)
 
 def useful_links(request):
