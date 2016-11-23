05afb4833139ad29e6a7664722bb4eb89e963e8f
Viach <vch.380@gmail.com>
2016-11-23 22:43:59 +0200

    work with views

--- quiz/views.py
+++ quiz/views.py
@@ -9,18 +9,17 @@ def index(request):
     return render(request, 'quiz/index.html', context)
 
 
-def quiz(request):
-    if request.session.get('quiz_status') == None:
+def quiz(request,*args):
+    status = args[0]
+    if status == 'start':
         request.session['current_question'] = 0
-        context = ''
+        context = {'info':"INFO about quiz"}
         return render(request, 'quiz/quiz_start.html', context)
-    elif request.session.get('quiz_status') == 'process':
-        request.session['current_question'] += 1
-        request.session['quiz_status'] = 'process'
-        context = {'quiz.question': quiz.questions[request.session['current_question']],}
+    elif int(status) in range(100):
+
+        context = {'quiz.question': quiz.questions[int(status)+1],}
         return render(request, 'quiz/quiz.html', context)
     else:
-        request.session['quiz_status'] = None
         request.session['current_question'] = 0
         context = {'result':'RESULT of quiz'}
         return render(request, 'quiz/quiz_finish.html', context)
