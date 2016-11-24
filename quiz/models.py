49d253c56be38e91f142eac4455b25a9154bb5ef
Viach <vch.380@gmail.com>
2016-11-24 14:34:10 +0200

    set of correct answers added to Quiz()

--- quiz/models.py
+++ quiz/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 class G(object):
     pass
+
 
 class CategoryQuestion(models.Model):
     name = models.CharField(max_length=200)
@@ -30,14 +32,16 @@ class Answer(models.Model):
     correct_answer = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return 'Name: ' + str(self.name) + '   Is correct: ' + str(self.correct_answer)
+
 
 
 class Quiz():
     def __init__(self, n=2):
         self.questions = Question.objects.all()[:n]
         for k, v in enumerate(self.questions):
-            self.questions[k].answers = set(Answer.objects.filter(question=v.id))
+            self.questions[k].answers = frozenset(Answer.objects.filter(question=v.id))
+            self.questions[k].correct_answers = {k.id for k in self.questions[k].answers if k.correct_answer }
 
     def __str__(self):
         return 'Set of Questions'
