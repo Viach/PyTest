5cd26cb04f118ba5c57394475a03aff61d1ffc71
Viach <vch.380@gmail.com>
2016-11-27 12:36:33 +0200

    new revision - answers in textField

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Question, Answer, CategoryQuestion, UsefulLinks
+from .models import Question, CategoryQuestion, UsefulLinks
 
 admin.site.register(CategoryQuestion)
 admin.site.register(Question)
-admin.site.register(Answer)
 admin.site.register(UsefulLinks)
--- quiz/models.py
+++ quiz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from  django.core.validators import validate_comma_separated_integer_list
 
 
 class G(object):
@@ -13,42 +14,38 @@ class CategoryQuestion(models.Model):
         return self.name
 
 
-class Answer(models.Model):
-    name = models.CharField(max_length=200)
-    explanation = models.TextField(blank=True)
-    code = models.TextField(blank=True)
-    ### question = models.ForeignKey(Question)
-    correct_answer = models.BooleanField(default=False)
-
-    def __str__(self):
-        return 'Name: ' + str(self.name) + '   Is correct: ' + str(self.correct_answer)
-
-
 class Question(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     code = models.TextField(blank=True)
     category = models.ForeignKey(CategoryQuestion)
-    answer = models.OneToOneField(Answer)
+    answers = models.TextField()
+    correct_answer = models.CharField(max_length=10, validators=[validate_comma_separated_integer_list])
+
     user_answer = None
 
     def __str__(self):
         return self.name
 
+    def get_answers(self):
+        return self.answers.split('\r\n')
+
+    def get_correct_answers(self):
+        return [int(i) for i in self.correct_answer.split(',')]
+
+    def get_input_type(self):
+        return 'radio' if len(self.get_correct_answers()) == 1 else 'checkbox'
+
 
 class Quiz():
     def __init__(self, n=2):
         self.questions = Question.objects.all()[:n]
-        for k, v in enumerate(self.questions):
-            self.questions[k].answer = frozenset(Answer.objects.filter(question=v.id))
-            self.questions[k].correct_answer = set(k.id for k in self.questions[k].answer if k.correct_answer)
-            self.questions[k].input_type = 'radio' if len(self.questions[k].correct_answer) == 1 else 'checkbox'
 
     def __str__(self):
         return 'Set of Questions'
 
     def result(self):
-        return [v.correct_answer == v.user_answer for v in self.questions]
+        return [v.get_correct_answer == v.user_answer for v in self.questions]
 
 
 class UsefulLinks(models.Model):
