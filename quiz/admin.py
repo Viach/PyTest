fd282580dfbb48cbaed6f925dd77e3ddc9413c26
Viach <vch.380@gmail.com>
2016-12-02 10:37:03 +0200

    admin Question customized

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import TextInput, Textarea
 
 from .models import Question, CategoryQuestion, UsefulLinks
 
@@ -6,11 +8,16 @@ admin.site.register(CategoryQuestion)
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    #fields = ['name', 'enabled']
-    list_filter = ['enabled']
+    fields = [ 'name', 'code', 'category', 'answers', 'correct_answer', 'explanation', 'enabled']
+    list_display = ['name', 'category', 'enabled', ]
+    list_editable = ['enabled', ]
+    list_filter = ['enabled', 'category', ]
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size': '80'})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 5, 'cols': 80})},
+    }
 
 
 admin.site.register(Question, QuestionAdmin)
-
 
 admin.site.register(UsefulLinks)
