b92f8057d7c9a4f48dadfb59b9a1c467412e138e
Viach <vch.380@gmail.com>
2016-12-10 10:49:19 +0200

    favicon added

--- quiz/models.py
+++ quiz/models.py
@@ -46,7 +46,7 @@ class Quiz():
     def __init__(self):
         self.questions = []
         self.categories = CategoryQuestion.objects.all()
-        for category in self.categories[:1]:
+        for category in self.categories:
             lst = list(Question.objects.all().filter(enabled=True).filter(category=category.id))
             shuffle(lst)
             self.questions.extend(lst[-2:])
